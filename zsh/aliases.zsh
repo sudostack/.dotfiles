80358fe5b3ae52161bf1e525ecca46b926460d4b
David Wu (sudostack) <dwu207@gmail.com>
2017-01-04 11:37:31 -0800

    [update] remove: aliases/; add: trrapigateway alias to zsh/aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,3 +4,6 @@ alias emacsc="emacsclient -c"
 
 # Ruby on Rails
 alias brake='bundle install&&bundle exec rake db:migrate&&bundle exec rake db:test:prepare'
+
+# The RealReal
+alias trrapigateway='/Users/davidwu/Desktop/trr/go/src/github.com/TheRealReal/therealreal-api'
