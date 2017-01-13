e14756d4bb8ca95ed0bae1868cd247611be8898f
David Wu (sudostack) <dwu207@gmail.com>
2017-01-13 10:50:35 -0800

    [add] alias for listing recent branches

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,9 @@ alias desk='cd ~/Desktop'
 alias ls='ls -Gp'
 alias emacsc="emacsclient -c"
 
+# Git
+alias grecent='for k in `git branch | perl -pe s/^..//`; do echo -e `git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k -- | head -n 1`\\t$k; done | sort'
+
 # Ruby on Rails
 alias brake='bundle install&&bundle exec rake db:migrate&&bundle exec rake db:test:prepare'
 
