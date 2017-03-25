ba9b0c775ed0a04d95ddea9ada561c1385e961c5
David Wu (sudostack) <dwu207@gmail.com>
2017-03-24 17:40:50 -0700

    Add git alias to delete merged branches

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,6 +5,7 @@ alias whatsmyip='curl ipinfo.io/ip'
 
 # Git
 alias grecent='for k in `git branch | perl -pe s/^..//`; do echo -e `git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k -- | head -n 1`\\t$k; done | sort'
+alias gbdmerged='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 
 # Ruby on Rails
 alias brake='bundle install&&bundle exec rake db:migrate&&bundle exec rake db:test:prepare'
