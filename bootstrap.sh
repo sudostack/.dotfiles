b360e357a88bb0a8169f24c86dfd91840f3294e5
David W <dwu207@gmail.com>
2019-09-28 10:51:58 -0700

    WIP: shell script for setting a new machine

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 1. clone dotfiles
+# 2. install homebrew
+# 3. asdf
+#   - python plugin -> upgrade pip ->
+# 4. neovim
+#   - symlink init.vim
+# 5. ripgrep
+# 6. fzf (fuzzy finder)
+# 7. upgrade tags (ctags)
