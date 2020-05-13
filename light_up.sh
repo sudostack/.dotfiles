0b615c5b25bd1bf441058d5a3e7477f9cf63897b
David W <dwu207@gmail.com>
2020-05-13 10:24:36 -0700

    REFACTOR bootstrap bash script

--- light_up.sh
+++ light_up.sh
@@ -24,35 +24,68 @@ fi
 # Homebrew
 if ! which brew; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  for utility in {asdf,direnv,fzf,github/gh/gh,glow,gnupg,htop,jq,nvim,ripgrep,tree}; do
-    brew install "$utility";
-  done
-
-  # Taps
-  brew install --HEAD universal-ctags/universal-ctags/universal-ctags
 else
   echo 'homebrew already installed'
 fi
 
+utilities=(
+  asdf                                            # language version manager (one to rule them all)
+  bash-completion                                 # unlike zsh (does not come out of the box)
+  direnv                                          # manage per-directory environment variables
+  fzf                                             # command-line fuzzy finder
+  github/gh/gh                                    # GitHub CLI
+  glow                                            # markdown reader
+  gnupg                                           # free OpenPGP (pretty good privacy standard)
+  htop                                            # improved top (interactive process viewer)
+  jq                                              # command-line JSON processor
+  nvim                                            # Neovim
+  ripgrep                                         # Rust implementation of grep (faster than Ag aka the Silver Searcher)
+  tree                                            # display directories as trees
+  universal-ctags/universal-ctags/universal-ctags # NOTE: may need to uninstall system version)
+  wget                                            # Internet file retriever
+)
+for utility in "${utilities[@]}"; do
+  if ! which "$utility"; then
+    echo "installing: ${utility}";
+    brew install "$utility";
+  fi
+done
+
+# Homebrew casks
+casks=(
+  ngrok
+)
+for cask in "${casks[@]}"; do
+  if ! which "$cask"; then
+    echo "installing: ${cask}";
+    brew cask install "$cask";
+  fi
+done
+
 # dotfiles
-if [ -d ~/.dotfiles ]; then
-  cd ~
-  for file in ~/.dotfiles/.{bashrc,bash_profile,exports,aliases,functions,utilities}; do
+for file in ~/.dotfiles/.{bashrc,bash_profile,exports,aliases,functions,utilities}; do
+  if [[ ! -e "$file" ]]; then
     [ -r "$file" ] && [ -f "$file" ] && ln -s "$file" .;
-  done
-else
-  echo 'dotfiles already exist'
-fi
+  fi
+done
 
 # Git
-ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
+for file in ~/.dotfiles/git/.{gitconfig,gitignore_global}; do
+  if [[ ! -e "$file" ]]; then
+    echo "copying: ${file}";
+    [ -r "$file" ] && [ -f "$file" ] && ln -s "$file" .;
+  fi
+done
 
 # Neovim config
-if [ ! -d ~/.config/nvim ]; then
+if [[ ! -d ~/.config/nvim ]]; then
+  echo 'copying neovim config';
   mkdir -p ~/.config/nvim && ln -s ~/.dotfiles/editor/init.vim ~/.config/nvim/init.vim
 fi
 
-# Vim Plug (Neovim)
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# vim-plug
+if [[ ! -e ~/.local/share/nvim/site/autoload/plug.vim ]]; then
+  echo 'installing vim-plug';
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim \
+    --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
